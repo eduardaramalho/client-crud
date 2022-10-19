e79ee9e749a93b0ed6f3aa215c09febb2bd325b8
eduardaramalho <eduardacns.ramalho@gmail.com>
2022-10-19 16:58:09 -0300

    add client

---
+++ malwee-main/client/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BlockUIModule } from 'ng-block-ui';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { DashComponent } from './dash/dash.component';
+import { RoutesModule } from './routes.module';
+import { GroupComponent } from './group/group.component';
+import { UserComponent } from './user/user.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    MenuComponent,
+    DashComponent,
+    GroupComponent,
+    UserComponent
+  ],
+  imports: [
+    RoutesModule,
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BlockUIModule.forRoot(),
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule
+  ],
+  exports : [
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ malwee-main/client/src/app/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  username : string = 'eduardaramalho';
+  password : string = 'pitico123';
+  hide : boolean = true;
+  isLogin : boolean = false;
+
+  constructor(private router : Router, private HttpClient : HttpClient) { }
+
+  ngOnInit(): void { 
+    setTimeout(() => {
+      this.login();
+    })
+  }
+
+  login(){
+    this.HttpClient.post('http://localhost:3003/logon', {username : this.username, password : this.password}).toPromise().then((response : any)=> {
+    console.log(response);
+      if(response.token){
+        this.isLogin = true;
+        window.localStorage.setItem('token', response.token);
+        
+        this.router.navigateByUrl('');
+        console.log("Logado");
+      }
+    })
+  }
+}
